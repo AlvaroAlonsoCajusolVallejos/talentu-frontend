0021c167f8dfc7c4aad8eade6ddf7b19e0be88b0
AlvaroCV1995 <30363055+AlvaroAlonsoCajusolVallejos@users.noreply.github.com>
2022-05-31 01:39:15 -0500

    a little change

--- src/components/Usuarios.vue
+++ src/components/Usuarios.vue
@@ -1,153 +1,140 @@
 <template>
-  <div class="hello">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col">
-          <button type="button" class="btn btn-primary" @click="modalToggle">
-            Agregar Nuevo Usuario
-          </button>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <table class="table table-striped">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Edad</th>
-                <th scope="col">Email</th>
-                <th scope="col">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="user in users" :key="user.id">
-                <td>{{ user.id }}</td>
-                <td>{{ `${user.first_name} ${user.last_name}` }}</td>
-                <td>
-                  {{ user?.birthday ? calcularEdad(user?.birthday) : "" }}
-                </td>
-                <td>{{ user.email }}</td>
-                <td>
-                  <button
-                    type="button"
-                    class="btn btn-warning"
-                    @click="modificar(user)"
-                  >
-                    Modificar
-                  </button>
-                  <button
-                    type="button"
-                    class="btn btn-danger"
-                    @click="eliminar(user)"
-                  >
-                    Eliminar
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-primary" @click="modalToggle">
+          Agregar Nuevo Usuario
+        </button>
       </div>
     </div>
-
-    <!-- modal -->
-    <div>
-      <div>
-        <div
-          ref="modal"
-          class="modal fade"
-          :class="{ show: active, 'd-block': active }"
-          tabindex="-1"
-          role="dialog"
-        >
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Datos del Usuario</h5>
+    <hr>
+    <div class="row">
+      <div class="col table-responsive">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Edad</th>
+              <th scope="col">Email</th>
+              <th scope="col">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="user in users" :key="user.id">
+              <td>{{ user.id }}</td>
+              <td>{{ `${user.first_name} ${user.last_name}` }}</td>
+              <td>
+                {{ user?.birthday ? calcularEdad(user?.birthday) : "" }}
+              </td>
+              <td>{{ user.email }}</td>
+              <td>
                 <button
                   type="button"
-                  class="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                  @click="modalToggle"
+                  class="btn btn-warning"
+                  @click="modificar(user)"
                 >
-                  <span aria-hidden="true">&times;</span>
+                  Modificar
                 </button>
-              </div>
-              <div class="modal-body">
-                <form
-                  class="row g-3 needs-validation"
-                  @submit.prevent="onSubmit"
+                <button
+                  type="button"
+                  class="btn btn-danger"
+                  @click="eliminar(user)"
                 >
-                  <div class="col-md-6">
-                    <label for="validationCustom01" class="form-label"
-                      >Nombres</label
-                    >
-                    <input
-                      type="text"
-                      class="form-control"
-                      v-model="user.first_name"
-                      required
-                    />
-                  </div>
-                  <div class="col-md-6">
-                    <label for="validationCustom02" class="form-label"
-                      >Apellidos</label
-                    >
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="validationCustom02"
-                      v-model="user.last_name"
-                      required
-                    />
-                  </div>
-                  <div class="col-md-7">
-                    <label for="validationCustomUsername" class="form-label"
-                      >Email</label
-                    >
-                    <div class="input-group has-validation">
-                      <span class="input-group-text" id="inputGroupPrepend"
-                        >@</span
-                      >
-                      <input
-                        type="email"
-                        class="form-control"
-                        id="validationCustomUsername"
-                        aria-describedby="inputGroupPrepend"
-                        v-model="user.email"
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div class="col-md-5">
-                    <label for="validationCustom03" class="form-label"
-                      >Fecha Nacimiento</label
-                    >
-                    <input
-                      type="date"
-                      class="form-control"
-                      id="validationCustom03"
-                      v-model="user.birthday"
-                      required
-                    />
-                  </div>
-                  <div class="col-12">
-                    <button class="btn btn-primary" type="submit">
-                      Guardar
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div v-if="active" class="modal-backdrop fade show"></div>
+                  Eliminar
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
-    <!-- modal -->
+    <hr>
   </div>
+
+  <!-- modal -->
+  <div
+    ref="modal"
+    class="modal fade"
+    :class="{ show: active, 'd-block': active }"
+    tabindex="-1"
+    role="dialog"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Datos del Usuario</h5>
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+            @click="modalToggle"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form class="row g-3 needs-validation" @submit.prevent="onSubmit">
+            <div class="col-md-6">
+              <label for="validationCustom01" class="form-label">Nombres</label>
+              <input
+                type="text"
+                class="form-control"
+                v-model="user.first_name"
+                required
+              />
+            </div>
+            <div class="col-md-6">
+              <label for="validationCustom02" class="form-label"
+                >Apellidos</label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="validationCustom02"
+                v-model="user.last_name"
+                required
+              />
+            </div>
+            <div class="col-md-7">
+              <label for="validationCustomUsername" class="form-label"
+                >Email</label
+              >
+              <div class="input-group has-validation">
+                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="validationCustomUsername"
+                  aria-describedby="inputGroupPrepend"
+                  v-model="user.email"
+                  required
+                />
+              </div>
+            </div>
+            <div class="col-md-5">
+              <label for="validationCustom03" class="form-label"
+                >Fecha Nacimiento</label
+              >
+              <input
+                type="date"
+                class="form-control"
+                id="validationCustom03"
+                v-model="user.birthday"
+                required
+              />
+            </div>
+            <div class="col-12">
+              <button class="btn btn-primary" type="submit">Guardar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-if="active" class="modal-backdrop fade show"></div>
+  <!-- modal -->
 </template>
 
 <script>
@@ -256,21 +243,3 @@ export default {
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
